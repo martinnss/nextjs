8f44beb9ba5f1120a7e3d3e858d025151e72c836
martinnss <martin.olivarest@utem.cl>
2025-01-14 00:03:57 -0300

    main 1 hora 6 min

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,6 +1,11 @@
 import AlbumList from '@/app/components/albumList'
 import React from 'react'
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: "about us",
+  description: "testing next ",
+};
 const page = () => {
   return (
     <div>
